db9f7138b34b6ea9e9be760a20796a37c3ebbda0
Tobias Bühler <tobias.buehler@t-online.de>
2020-04-15 10:58:35 +0200

    insert a dialog for displaying error messages to the users

---
+++ Simulator/src/ErrorDialog.java
@@ -0,0 +1,70 @@
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+
+public class ErrorDialog extends JDialog {
+
+	private final JPanel contentPanel = new JPanel();
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		try {
+			ErrorDialog dialog = new ErrorDialog();
+			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+			dialog.setVisible(true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Create the dialog.
+	 */
+	public ErrorDialog() {
+		setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+		setBounds(100, 100, 639, 301);
+		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPanel, BorderLayout.NORTH);
+		contentPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		{
+			JLabel lbl_ErrorTitle = new JLabel("\u00DCberschrift");
+			contentPanel.add(lbl_ErrorTitle);
+		}
+		{
+			JPanel panel = new JPanel();
+			getContentPane().add(panel, BorderLayout.CENTER);
+			{
+				JLabel lbl_ErrorText = new JLabel("Text");
+				panel.add(lbl_ErrorText);
+				lbl_ErrorText.setFont(new Font("Tahoma", Font.PLAIN, 16));
+			}
+		}
+		{
+			JPanel buttonPane = new JPanel();
+			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
+			getContentPane().add(buttonPane, BorderLayout.SOUTH);
+			{
+				JButton okButton = new JButton("OK");
+				okButton.setActionCommand("OK");
+				buttonPane.add(okButton);
+				getRootPane().setDefaultButton(okButton);
+			}
+			{
+				JButton cancelButton = new JButton("Cancel");
+				cancelButton.setActionCommand("Cancel");
+				buttonPane.add(cancelButton);
+			}
+		}
+	}
+
+}
