b432d193bda70bc7a3a1702a2b5e2f22f454f6ae
Toni <toni.einecker@gmail.com>
2020-05-27 10:43:21 +0200

    Update doxygen commands of Timer.java and Watchdog.Watchdog.java

--- Simulator/src/Watchdog.java
+++ Simulator/src/Watchdog.java
@@ -8,20 +8,27 @@
  * 
  * */
 public class Watchdog {
-	
+	/// The object of the main Controller class.
 	private Controller ctr;
-	
+	/// Internal counter to counter if a prescaler is assigned.
 	private int preScaler;
+	/// Saving a time stamp to compare against, to get the 18ms timeout.
 	private double timeStamp;
-	int timeOutPeriod = 10;
-	public boolean exit = false;
-	
+
+	/**
+	 * The constructor. Assigning the Controller object and setting an initial time stamp.
+	 * @param pCtr
+	 */
 	public Watchdog(Controller pCtr) 
 	{
 		this.ctr = pCtr;
 		this.timeStamp = this.ctr.getOperationalTime();
 	}
 	
+	/**
+	 * Method to check if a 18ms operational time timeout occurred. If so, the timestamp variable is reset and the watchdog incremented.
+	 * @param timeNow
+	 */
 	public void update(double timeNow) {
 		if(ctr.getMemory().get_WDTE() == 1 && (timeNow - this.timeStamp) >= 18.0) {
 			incrementWatchDog();
@@ -29,6 +36,10 @@ public class Watchdog {
 		}
 	}
 	
+	/**
+	 * Method to check if an watchdog timeout (considering the prescaler) happened.
+	 * If the prescaler is not assigned to the watchdog or the prescaler variable is full, the TO bit and status register is cleared and the processor is waked up or reset.
+	 */
 	protected void incrementWatchDog() 
 	{
 		System.out.println(this.preScaler);
@@ -49,12 +60,20 @@ public class Watchdog {
 		}
 	}
 	
-	protected void setPreScaler(int preScaler) {
-		this.preScaler = preScaler;
-	}
-	
+	/**
+	 * Method to reset the watchdog. 
+	 * The timestamp is set to the actual operational time and the prescaler is set to 0.
+	 */
 	protected void reset() {
 		this.timeStamp = this.ctr.getOperationalTime();
 		this.preScaler = 0;
+	}
+	
+	/**
+	 * The Setter for the preScaler variable.
+	 * @param preScaler The preScaler to set.
+	 */
+	protected void setPreScaler(int preScaler) {
+		this.preScaler = preScaler;
 	}
 }
